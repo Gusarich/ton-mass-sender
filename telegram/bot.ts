14e5d99943e5d15bc033eda93d790d8b41dfe1a7
Gusarich <danielsedovzzz@gmail.com>
2023-04-13 09:23:03 +0300

    allow text messages

--- telegram/bot.ts
+++ telegram/bot.ts
@@ -21,6 +21,60 @@ compile('MassSender').then((c) => {
     Object.freeze(code);
 });
 
+async function sendTransaction(messages: Msg[], chatId: number, provider: TonConnectProvider) {
+    const data = massSenderConfigToCell({
+        messages: messages,
+    });
+
+    const address = contractAddress(0, {
+        code,
+        data,
+    });
+    const value =
+        messages.map((msg) => msg.value).reduce((a, b) => a + b) +
+        BigInt(messages.length + Math.ceil(messages.length / 254)) * toNano('0.1');
+
+    await bot.sendMessage(chatId, 'Confirm transaction in Tonkeeper');
+
+    try {
+        await provider.sendTransaction(address, value, undefined, {
+            code,
+            data,
+        });
+        await bot.sendMessage(chatId, 'Transaction sent!');
+    } catch (UserRejectsError) {
+        await bot.sendMessage(chatId, 'You rejected the transaction');
+    }
+}
+
+async function processMessages(messages: Msg[], chatId: number) {
+    const provider = new TonConnectProvider(getConnector(chatId), 'Tonkeeper');
+    await provider.restoreConnection();
+
+    if (!provider.address()) {
+        await bot.sendMessage(chatId, 'Connect your Tonkeeper wallet');
+
+        const url = await provider.getConnectUrl();
+
+        if (url === undefined) {
+            return;
+        }
+
+        const filename = os.tmpdir() + 'qrcode' + Math.floor(Math.random() * 1e6).toString() + '.png';
+        toFile(filename, url, async () => {
+            const msg = await bot.sendPhoto(chatId, filename, { caption: 'Scan this QR code with Tonkeeper' });
+            await fs.promises.rm(filename);
+            await provider.connect(async () => {
+                await bot.deleteMessage(chatId, msg.message_id);
+                await bot.sendMessage(chatId, 'Wallet connected!');
+                await sendTransaction(messages, chatId, provider);
+            });
+        });
+    } else {
+        await sendTransaction(messages, chatId, provider);
+    }
+}
+
 async function main(): Promise<void> {
     await initRedisClient();
 
@@ -47,52 +101,27 @@ async function main(): Promise<void> {
             });
         }
 
-        const data = massSenderConfigToCell({
-            messages: messages,
-        });
+        await processMessages(messages, chatId);
+    });
 
-        const provider = new TonConnectProvider(getConnector(chatId), 'Tonkeeper');
-        await provider.restoreConnection();
+    bot.on('text', async (msg) => {
+        const chatId = msg.chat.id;
 
-        if (!provider.address()) {
-            await bot.sendMessage(chatId, 'Connect your Tonkeeper wallet');
-
-            const url = await provider.getConnectUrl();
-
-            if (url === undefined) {
-                return;
-            }
-
-            const filename = os.tmpdir() + 'qrcode' + Math.floor(Math.random() * 1e6).toString() + '.png';
-            toFile(filename, url, async () => {
-                const msg = await bot.sendPhoto(chatId, filename, { caption: 'Scan this QR code with Tonkeeper' });
-                await fs.promises.rm(filename);
-                await provider.connect(() => {
-                    bot.deleteMessage(chatId, msg.message_id);
-                    bot.sendMessage(chatId, 'Wallet connected!');
-                });
-            });
-        } else {
-            const address = contractAddress(0, {
-                code,
-                data,
-            });
-            const value =
-                messages.map((msg) => msg.value).reduce((a, b) => a + b) +
-                BigInt(messages.length + Math.ceil(messages.length / 254)) * toNano('0.1');
-
-            await bot.sendMessage(chatId, 'Confirm transaction in Tonkeeper');
-
-            try {
-                await provider.sendTransaction(address, value, undefined, {
-                    code,
-                    data,
-                });
-                await bot.sendMessage(chatId, 'Transaction sent!');
-            } catch (UserRejectsError) {
-                await bot.sendMessage(chatId, 'You rejected the transaction');
-            }
+        const rawMessagesText = msg.text!.match(/[a-zA-Z0-9-_]{48}: \d+/gm);
+        if (rawMessagesText == null || rawMessagesText.length == 0) {
+            return;
         }
+        const rawMessages = rawMessagesText.map((t) => t.split(': '));
+
+        let messages: Msg[] = [];
+        for (const msg of rawMessages) {
+            messages.push({
+                value: toNano(msg[1]),
+                destination: Address.parse(msg[0]),
+            });
+        }
+
+        await processMessages(messages, chatId);
     });
 }
 
